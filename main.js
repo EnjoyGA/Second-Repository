c0c85cf1cbed3dddb73a3a505a4da241f1165fe6
virab <example@gmail.com>
2022-06-01 16:36:16 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// get init - start follow the project
+// get config --local user.name "virab" - reg
+// get config --local user.email example@gmail.com - reg
+// git status - status of files (active, not active)
+// git add -A - active all files
